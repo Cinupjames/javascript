b62582550a82ff60a42a26f3db09e7809270638a
Cinupjames <125945019+Cinupjames@users.noreply.github.com>
2023-02-22 22:17:46 +0530

    cipher problem added

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,21 +1,18 @@
-
 //remove duplicate from string
 
-
 // const removeDuplicate = (S) => {
-  
+
 //    const newString  = S.split("");
 //    const removeDuplicate=  [...new Set(newString)]
 //    return removeDuplicate.join("");
 // }
 // console.log(removeDuplicate("aaaaabbbbbb"));
 
-
-// //Diagonal Difference 
+// //Diagonal Difference
 
 // function diagonalDifference(arr) {
-  
-//     let n = arr.length; 
+
+//     let n = arr.length;
 //     console.log(n)
 //     let d1 = 0;
 //     let d2 = 0;
@@ -23,55 +20,38 @@
 //            d1 += arr[i][i];
 
 //             d2 += arr[i][n-1-i];
-         
+
 //       }
-  
+
 //   return Math.abs(d1 - d2);
 // }
 
 //  console.log(diagonalDifference( [[1,2,3],[4,5,6],[9,8,9]]))
 
- //cipher
+//cipher
 
- function ceaserCipher(str,num){
-   num=num%26;
-   console.log(num,"jhj");
-   str=str.toLowerCase();
+function ceaserCipher(str, num) {
+  num = num % 26;
+  console.log(num, "jhj");
+  str = str.toLowerCase();
 
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const rotatedAlphabet = alphabet.slice(num) + alphabet.slice(0, num);
 
+  let result = "";
 
-   const alphabet ='abcdefghijklmnopqrstuvwxyz';
-   const rotatedAlphabet =alphabet.slice(num)+alphabet.slice(0,num);
-console.log(rotatedAlphabet);
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    let newChar = currentChar;
 
-   let result='';
-     
+    if (alphabet.includes(currentChar)) {
+      const index = alphabet.indexOf(currentChar);
 
-   for(let i=0;i<str.lenght;i++){
-      const currentChar=str[i];
-      let newChar = currentChar;
-
-
-      if(alphabet.includes(currentChar)){
-         const index=alphabet.indexOf(currentChar);
-         console.log(index,"res");
-         newChar=rotatedAlphabet[index];
-         console.log(newChar,"res");
-      }
-result +=newChar
-console.log(result,"res");
-   }
-return result
- }
-const input="There’s-a-starman";
-const output=ceaserCipher(input,3);
-console.log(output);
-
-
-
-
-
-
-
-
-
+      newChar = rotatedAlphabet[index];
+    }
+    result += newChar;
+  }
+  return result;
+}
+const input = "There’s-a-starman";
+const output = ceaserCipher(input, 3);
